9a6648ee4ad2669a41941faaebf72ef74fc3ddeb
Jan Miszura <janmiszura@gmail.com>
2018-12-30 06:21:45 -0200

    bugfix no test de quantidade de inputs

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -16,7 +16,7 @@ class SubscribeTest(TestCase):
     def test_html(self):
         """must contais input tags"""
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, 'input', 6)
+        self.assertContains(self.resp, 'input type', 6)
         self.assertContains(self.resp, 'type="text"', 3)
         self.assertContains(self.resp, 'type="email"')
         self.assertContains(self.resp, 'type="submit"')
