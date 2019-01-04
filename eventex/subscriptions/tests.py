00f3a5b78a27f8d005da247d2d97975b94581861
Jan Miszura <janmiszura@gmail.com>
2019-01-04 15:54:22 -0200

    enviando e-mail de inscrição

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core import mail
 from eventex.subscriptions.forms import SubscriptionForm
 
 class SubscribeTest(TestCase):
@@ -29,3 +30,39 @@ class SubscribeTest(TestCase):
         """Context must have subscription form"""
         form = self.resp.context['form']
         self.assertIsInstance(form, SubscriptionForm)
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(name='Jan Miszura', cpf='12345678901', 
+                email='janmiszura@gmail.com', phone='62999999999')
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        """Valid post should redirect to /inscricao/"""
+        self.assertEqual(302, self.resp.status_code)
+    
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+    
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expected = 'Confirmação de Inscrição'
+        self.assertEqual(expected, email.subject)
+    
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expected = 'contato@eventex.com.br'
+        self.assertEqual(expected, email.from_email)
+    
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expected = ['contato@eventex.com.br', 'janmiszura@gmail.com']
+        self.assertEqual(expected, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        self.assertIn('Jan Miszura', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('janmiszura@gmail.com', email.body)
+        self.assertIn('62999999999', email.body)
